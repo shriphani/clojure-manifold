151d49821effbd44847c986d9b6b805850fd5a70
Shriphani Palakodety <shriphanip@gmail.com>
2014-10-30 03:43:51 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure_manifold
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_manifold/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-manifold.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-manifold.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
